69d55a03bf3a3723e6b26fa9fbf61c767ffaa6c5
simapatil19 <sima.patil19@gmail.com>
2026-03-18 16:40:56 +0530

    Added framework structure for Maven BDD

---
+++ src/main/java/Hybrid/Hybrid/App.java
@@ -0,0 +1,7 @@
+package Hybrid.Hybrid;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/Hybrid/Hybrid/AppTest.java
@@ -0,0 +1,13 @@
+package Hybrid.Hybrid;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
